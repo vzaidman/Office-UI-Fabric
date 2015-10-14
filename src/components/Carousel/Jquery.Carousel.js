2ab420f1adc66c10bdd066a86ea15126afcd8ba3
John Miller <rubixws@gmail.com>
2015-10-13 17:49:03 -0700

    - Removed extra code - Changed sample images.

--- src/components/Carousel/Jquery.Carousel.js
+++ src/components/Carousel/Jquery.Carousel.js
@@ -12,16 +12,33 @@
         	var $paginationButton = $pagination.find('.ms-Carousel-paginationButton').clone(); // Clone a copy of the example pagination
         	var $rightButton = $thisCarousel.find('.ms-Carousel-rightArrow');
         	var $leftButton = $thisCarousel.find('.ms-Carousel-leftArrow');
-        	var $slideWidth = 955;
+        	var $staticContent = $thisCarousel.find('.ms-Carousel-staticContent');
+        	var $slideWidth = 830;
         	var currentSlideNum;
         	var prevSlideNum;
         	var nextSlideNum;
         	var autoSlide = true;
         	var timeoutId;
-        	var slideTiming = 2000;
+        	var slideTiming = 1000;
+			var slidingTime = 1000;
         	var cSlideLeftPos;
         	var pSlideLeftPos;
         	var nSlideLeftPos;
+
+        	var slideLeftOut40 = 'ms-u-slideLeftOut40';
+        	var slideLeftIn40 = 'ms-u-slideLeftIn40';
+        	var slideRightOut40 = 'ms-u-slideRightOut40';
+        	var slideRightIn40 = 'ms-u-slideRightIn40';
+
+        	var slideLeftOut80 = 'ms-u-slideLeftOut80';
+        	var slideLeftIn80 = 'ms-u-slideLeftIn80';
+        	var slideRightOut80 = 'ms-u-slideRightOut80';
+        	var slideRightIn80 = 'ms-u-slideRightIn80';
+
+        	var isHiddenClass = 'is-hidden';
+
+        	var animationLayerOneClass = "ms-Carousel-animationLayerOne";
+        	var animationLayerTwoClass = "ms-Carousel-animationLayerTwo";
 
         	// Find all slides and store them
         	function prepare() {
@@ -44,8 +61,6 @@
         			clearInterval(timeoutId); // Maybe the user will want to control it themselves. 
         		});
 
-        		// Pagination
-
         	}
 
         	function startAutoSlide() {
@@ -59,9 +74,24 @@
         		var $paginationButtonClone = $paginationButton.clone();
     			var $paginationClass = "ms-Carousel-paginationButton-" + slideNumber;
     			// Setup pagination
-    		
+    			
+    			$paginationButtonClone.click(function() {
+    				moveSlideTo(slideNumber);
+    			});
+
     			//Add pagination button for the current slide
     			$pagination.append($paginationButtonClone.addClass($paginationClass).attr('data-slide', slideNumber));
+
+        	}
+
+        	function setActivePagination(slideNumber) {
+
+        		// Remove all Active classes
+        		var $pButtons = $thisCarousel.find('.ms-Carousel-paginationButton');
+        		var $pButton = $pButtons.eq(slideNumber);
+
+        		$pButtons.removeClass('is-active');
+        		$pButton.addClass('is-active');
 
         	}
 
@@ -84,6 +114,7 @@
 	        		});
 
 	        		setCurrentSlide(0);
+	        		setActivePagination(0);
 	        		settupPrevNextSlides();
 
 	        	} else {
@@ -93,6 +124,27 @@
 
 	        function setCurrentSlide(slideNumber) {
 	        	currentSlideNum = slideNumber;
+	        }
+
+	        function moveSlideTo(slideNumber) {
+
+	        	if(currentSlideNum != slideNumber) {
+
+		        	if (slideNumber > currentSlideNum) {
+		        		// Make the next slide the new one and move them
+		        		setNextSlide(slideNumber);
+		        		animateAllSlides("right");
+		        	} else {
+		        		// Make the next slide the new one and move them
+		        		setPreviousSlide(slideNumber);
+		        		animateAllSlides("left");
+		        	}
+
+		        	setCurrentSlide(slideNumber);
+        			setActivePagination(slideNumber);
+        			settupPrevNextSlides();
+		        }
+
 	        }
 
 	        function moveSlide(directionAction) {
@@ -109,12 +161,10 @@
 	        			animateAllSlides("right");
 	        			setCurrentSlide(currentSlideNum + 1);
 	        		}
-
+	        		setActivePagination(currentSlideNum);
 	        		settupPrevNextSlides();
-	        		positionNextSlide();
 
 	        	} else if(directionAction == "previous") {
-	        		
 	        		
 	        		if((currentSlideNum - 1) < 0) {
 	        			animateAllSlides("left");
@@ -124,8 +174,8 @@
 	        			setCurrentSlide(currentSlideNum - 1);
 	        		}
 
+	        		setActivePagination(currentSlideNum);
 	        		settupPrevNextSlides();
-	        		positionNextSlide();
 	        	
 	        	}
 
@@ -133,96 +183,89 @@
 
 	        // Animation Functions
 	        function animateAllSlides(direction) {
-	        	
+
+	        	var $cSlide = $slides.eq(currentSlideNum);
+	        	var $cSlideI = ($cSlide.index() - 1);
+	        	var $nSlide = $slides.eq(nextSlideNum);
+	        	var $pSlide = $slides.eq(prevSlideNum);
+
 	        	if(direction == "right") {
 
-	        		// Animate Current Slide
-	        		$slides.eq(currentSlideNum).stop();
-	        		$slides.eq(currentSlideNum).animate({
-	        			"left": pSlideLeftPos
-	        		}, 1000, function() {
-	        			// Afterwards reset the previous slides styles
-        				clearSlideStyles(prevSlideNum);
+					// Fade out Current Slides LayerOne
+	        		$cSlide.find('.' + animationLayerOneClass).addClass(slideLeftOut80);
+	        		$cSlide.find('.' + animationLayerTwoClass).addClass(slideLeftOut40);
+
+	        		// Add new Background color
+	        		$thisCarousel.css({
+	        			'background-color': $nSlide.data('bgcolor')
 	        		});
 
-	        		// Animate Next Slide
-	        		$slides.eq(nextSlideNum).stop();
-	        		$slides.eq(nextSlideNum).show().animate({
-	        			"left": cSlideLeftPos
-	        		}, 1000, function() {
+	        		setTimeout(function() {
 
-	        		});
+	        			// Clean out the current styles
+	        			clearSlideStyles($cSlideI);
+
+	        			console.log();
+
+	        			// Add Classes to next slide
+		        		$nSlide.show();
+
+		        		$nSlide.find('.' + animationLayerOneClass).addClass(slideLeftIn80);
+	        			$nSlide.find('.' + animationLayerTwoClass).addClass(slideLeftIn40);
+
+
+	        		}, 300);
+		        		
+
+	        		// $nSlide.find('.' + animationLayerTwoClass);
 
 	        	} else if(direction == "left") {
 
-	        		// Animate Current Slide
-	        		$slides.eq(currentSlideNum).stop();
-	        		$slides.eq(currentSlideNum).animate({
-	        			"left": nSlideLeftPos
-	        		}, 1000, function() {
-	        			// Afterwards reset the previous slides styles
-	        			clearSlideStyles(nextSlideNum);
+	        		$cSlide.find('.' + animationLayerOneClass).addClass(slideRightOut80);
+	        		$cSlide.find('.' + animationLayerTwoClass).addClass(slideRightOut40);
+
+	        		// Add new Background color
+	        		$thisCarousel.css({
+	        			'background-color': $pSlide.data('bgcolor')
 	        		});
 
-	        		// Animate Next Slide
-	        		$slides.eq(prevSlideNum).stop();
-	        		$slides.eq(prevSlideNum).show().animate({
-	        			"left": cSlideLeftPos
-	        		}, 1000, function() {
-	        		});
+	        		setTimeout(function() {
+
+	        			// Clean out the current styles
+	        			clearSlideStyles($cSlideI);
+
+	        			console.log($cSlideI);
+
+	        			// Add Classes to next slide
+		        		$pSlide.show();
+
+		        		$pSlide.find('.' + animationLayerOneClass).addClass(slideRightIn80);
+	        			$pSlide.find('.' + animationLayerTwoClass).addClass(slideRightIn40);
+
+
+	        		}, 300);
 	        		
 	        	}
 	        }
 
-	        function animateInRight(slideNumber, animationClass) {
-	        	$slides.eq(slideNumber).addClass(animationClass);
-	        }
-
-	        function animateOutRight(slideNumber, animationClass) {
-	        	$slides.eq(slideNumber).addClass(animationClass);
-	        }
-
-	        function animateInLeft(slideNumber, animationClass) {
-	        	$slides.eq(slideNumber).addClass(animationClass);
-	        }
-
-	        function animateOutLeft(slideNumber, animationClass) {
-	        	$slides.eq(slideNumber).addClass(animationClass);
-	        }
-
-	        // Position Slide Functionality
-
-	        function positionCurrentSlide() {
-	        	//Show current slide
-	        	$slides.eq(currentSlideNum).css({
-	        		"left": cSlideLeftPos
-	        	});
-	        }
-
-	        function positionNextSlide() {
-	     		// Show Next Slide
-        		$slides.eq(nextSlideNum).css({
-	        		"left": nSlideLeftPos
-	        	});
-	        }
-
-	       	function positionPrevSlide() {
-	       		// Position previous slide
-	        	$slides.eq(prevSlideNum).css({
-	        		"left": pSlideLeftPos
-	        	});
-	       	}
-
-	       	function positionAllSlides() {
-	       		positionCurrentSlide();
-	       		positionNextSlide();
-	       		positionPrevSlide();
-	       	}
-
 	       	// Clear positions
 	       	function clearSlideStyles(slideNumber) {
-	       		$slides.eq(slideNumber).removeAttr('style').hide();
-	       		// $slides.eq(slideNumber).hide();
+
+	       		$slides.eq(slideNumber).find('.' + animationLayerOneClass)
+	       			.removeClass(slideLeftOut80)
+	       			.removeClass(slideLeftIn80)
+	       			.removeClass(slideRightIn80) 
+	       			.removeClass(slideRightOut80)
+	       			.addClass(isHiddenClass);
+
+	       		$slides.eq(slideNumber).find('.' + animationLayerTwoClass)
+	       			.removeClass(slideLeftOut40)
+	       			.removeClass(slideLeftIn40)
+	       			.removeClass(slideRightIn40) 
+	       			.removeClass(slideRightOut40)
+	       			.addClass(isHiddenClass);
+
+	       		$slides.eq(slideNumber).hide();
 	       	}
 
 	        function calcSlidePositions(currentSlide) {
@@ -237,24 +280,32 @@
 
 	        	// Set static positions
 	        	cSlideLeftPos = parentCenterWidth - slideCenterWidth;
-	        	pSlideLeftPos = (cSlideLeftPos - $slideWidth) - cSlideLeftPos;
-	        	nSlideLeftPos = (cSlideLeftPos + $slideWidth) + cSlideLeftPos;
+	        	pSlideLeftPos = -(cSlideLeftPos + $slideWidth);
+	        	nSlideLeftPos = cSlideLeftPos + $slideWidth + cSlideLeftPos;
+	        }
+
+	        function setPreviousSlide(slideNumber) {
+	        	prevSlideNum = slideNumber;
+	        }
+
+	        function setNextSlide(slideNumber) {
+	        	nextSlideNum = slideNumber;
 	        }
 
 	        function settupPrevNextSlides() {
 
 	        	// Setup The Next Slide Index
         		if(currentSlideNum + 1 > ($slides.length - 1)) {
-	        		nextSlideNum = 0;
+	        		setNextSlide(0);
 	        	} else {
-	        		nextSlideNum = currentSlideNum + 1;
+	        		setNextSlide(currentSlideNum + 1);
 	        	}
-	        	
+
 	        	// Setup the Previous Slide Index
 	        	if(currentSlideNum - 1 < 0) {
-	        		prevSlideNum = ($slides.length - 1);
+	        		setPreviousSlide($slides.length - 1);
 	        	} else {
-	        		prevSlideNum = currentSlideNum - 1;
+	        		setPreviousSlide(currentSlideNum - 1);
 	        	}
 
 	        }
@@ -264,7 +315,6 @@
 	        	buildSlides();
 	        	currentSlideNum = 0;
 	        	calcSlidePositions(currentSlideNum);
-	        	positionAllSlides();
 	        	buildControls();
 	        }
 
